5e2ac682b7ddf7b73f2b458bd06bb999c70d7c48
Akarikev <princeelorm17@gmail.com>
2023-10-06 23:46:42 +0000

    mod: layout formatted

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,38 +8,37 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "reuseOnline ⚡ - Easily check your users online status",
-  description : "A react hook to easily check users online status of your users", 
-  icons : {
-    icon : '/favicon.ico', 
-    shortcut: '/favicon.ico', 
-    apple : '/favicon.ico', 
-  
-  }, 
-  applicationName : "reuseOnline Hook ⚡", 
-  keywords : [
+  description: "A react hook to easily check users online status of your users",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+  applicationName: "reuseOnline Hook ⚡",
+  keywords: [
     "react custom hook",
-    "online activity hook", 
-    "react useonline", 
-    "reuseOnline", 
-    "react hook to check online status", 
-    "online status", 
-    "users online status"
-  ], 
-  twitter : {
-    card: 'summary_large_image', 
-    title : "reuseOnline ⚡ - Easily check your users online status ", 
-    description : "A react hook to easily check users online status of your users", 
-    creator:"@elorm_elom", 
-    images: ["https://reuseonline.vercel.app/twitter-image.png"]
+    "online activity hook",
+    "react useonline",
+    "reuseOnline",
+    "react hook to check online status",
+    "online status",
+    "users online status",
+  ],
+  twitter: {
+    card: "summary_large_image",
+    title: "reuseOnline ⚡ - Easily check your users online status ",
+    description:
+      "A react hook to easily check users online status of your users",
+    creator: "@elorm_elom",
+    images: ["https://reuseonline.vercel.app/twitter-image.png"],
+  },
 
-  }, 
-
-  openGraph : {
-    images: "https://reuseonline.vercel.app/opengraph-image.png", 
-    title: "reuseOnline ⚡ - Easily check your users online status", 
-    description : "A react hook to easily check users online status of your users", 
-  }
-  
+  openGraph: {
+    images: "https://reuseonline.vercel.app/opengraph-image.png",
+    title: "reuseOnline ⚡ - Easily check your users online status",
+    description:
+      "A react hook to easily check users online status of your users",
+  },
 };
 
 export default function RootLayout({
@@ -49,7 +48,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-     
       <body className={cn("min-h-screen  antialiased grainy", inter.className)}>
         <NavBar />
         {children}
