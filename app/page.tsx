721e71f2c1b8ef4b624d31abe32c039ecd054d01
Akarikev <princeelorm17@gmail.com>
2023-10-06 21:12:38 +0000

    finished

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center ">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
           <p className="text-sm font-semibold text-gray-700 cursor-pointer ">
-            <code>reuseonline -v 1.1.6</code>
+            <code>reuseonline is live 🎉</code>
           </p>
         </div>
 
@@ -97,6 +97,21 @@ export default function Home() {
         </div>
       </div>
 
+      <div>
+        <div
+          aria-hidden="true"
+          className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 "
+        >
+          <div
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50% -13rem) ] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"
+          />
+        </div>
+      </div>
+
       <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
         <div className="mb-12 px-6 lg:px-8">
           <div className="mx-auto max-w-2xl sm:text-center">
@@ -110,11 +125,24 @@ export default function Home() {
                 navigator.onLine
               </code>{" "}
               property to determine the initial online status of the user's
-              device. It also adds event listeners for the <code className="bg-green-600 p-1 rounded-md text-white">online</code>{" "}
-              and <code className="bg-red-600 p-1 rounded-md text-white">offline</code> events to update the online status
-              dynamically.
+              device. It also adds event listeners for the{" "}
+              <code className="bg-green-600 p-1 rounded-md text-white">
+                online
+              </code>{" "}
+              and{" "}
+              <code className="bg-red-600 p-1 rounded-md text-white">
+                offline
+              </code>{" "}
+              events to update the online status dynamically.
             </p>
           </div>
+        </div>
+      </div>
+
+      <div className="mx-auto flex justify-center p-6 backdrop-blur-0 bg-gray-500/10">
+        <div className="flex-col">
+          <p className="font-semibold">made by prince elorm with 💖</p>
+          <p className="font-semibold">@ princeelorm17@gmail.com</p>
         </div>
       </div>
     </>
