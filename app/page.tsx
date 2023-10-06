0345795c4f590c820f40859e1655c3240946e4bf
Akarikev <princeelorm17@gmail.com>
2023-10-06 20:10:48 +0000

    info section

--- app/page.tsx
+++ app/page.tsx
@@ -45,9 +45,7 @@ export default function Home() {
         </p>
         <div className="mt-7 mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-md border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
           <p className="text-sm font-semibold text-gray-700 cursor-pointer ">
-            <pre>
-              <code>npm i re-use-online</code>
-            </pre>
+            <code>npm i re-use-online</code>
           </p>
         </div>
       </MaxWidthWrapper>
@@ -66,8 +64,59 @@ export default function Home() {
               className="relative left-[calc(50% -11rem) ] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             />
           </div>
-</div>
+          <div>
+            <div className="mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <Image
+                    src="/reusehook.png"
+                    width={1349}
+                    height={563}
+                    alt="product preview"
+                    quality={100}
+                    className="rounded-md bg-white w-full object-contain p-2 sm:p-4 md:p-4 shadow-2xl ring-1 ring-gray-900/10"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div
+              aria-hidden="true"
+              className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 "
+            >
+              <div
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+                className="relative left-[calc(50% -13rem) ] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"
+              />
+            </div>
+          </div>
         </div>
+      </div>
+
+      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+              How it <span className="text-blue-600 font-bold">Works</span>{" "}
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">
+              {" "}
+              The useOnlineStatus hook utilizes the{" "}
+              <code className="bg-gray-800 p-1 rounded-md text-white">
+                navigator.onLine
+              </code>{" "}
+              property to determine the initial online status of the user's
+              device. It also adds event listeners for the <code className="bg-green-600 p-1 rounded-md text-white">online</code>{" "}
+              and <code className="bg-red-600 p-1 rounded-md text-white">offline</code> events to update the online status
+              dynamically.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- components/Codeblock.tsx
+++ components/Codeblock.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 const Codeblock = ({ code }: any) => {
     return (
-      <div className="text-left">
+      <div className="text-left" suppressHydrationWarning>
         <pre>
           <code>{code}</code>
         </pre>
